8a3c2ac6a9aad080c4cb4d3ab9b77bc905688b45
Adrian Garcia <adrian.garcia@mercadolibre.com.co>
2021-06-21 14:34:31 -0500

    mees

--- Receiver/src/main/java/com/example/division/controller/OperationController.java
+++ Receiver/src/main/java/com/example/division/controller/OperationController.java
@@ -1,6 +1,8 @@
 package com.example.division.controller;
 
 import com.example.division.service.IOperationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,13 @@ public class OperationController {
     @Autowired
     IOperationService operationService;
 
+    private static final Logger logger = LoggerFactory.getLogger(OperationController.class);
+
     @GetMapping("/test1")
     public ResponseEntity<Double> test1(){
 
         Double result = operationService.returnRandomProbability();
+        logger.info("test1 executed");
         if( result > 0.3 ){
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }else{
@@ -29,6 +34,7 @@ public class OperationController {
     public ResponseEntity<Double> test2() throws InterruptedException {
 
         Double result = operationService.returnRandomProbability();
+        logger.info("test2 executed");
         Thread.sleep((long) (result * 5000));
         return new ResponseEntity<>(result, HttpStatus.OK);
 
